af4277da784d28f0a8486dbe0a6043d2b86a1ce4
Claudia Ramirez <claudiadrc2@gmail.com>
2026-08-11 20:07:40 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-0d238d93-d573-408d-8efa-5d4954b13d12.containerhub.tripleten-services.com"
+
+DOC_PATH = "/docs/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
